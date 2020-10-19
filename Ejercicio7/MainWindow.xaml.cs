6b6270941a2a8624bad9a916724473f88007d30b
Alextico 612 <alextico612@gmail.com>
2020-10-19 18:18:52 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio7/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio7
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            primerRadioButton.IsChecked = true;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            textoTextBlock.FontSize = double.Parse((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
